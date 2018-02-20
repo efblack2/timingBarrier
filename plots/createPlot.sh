0003b3e02c44bc20d2256c47d5f67e7e15faf1cb
Edgar F. Black <edgarfblack@gmail.com>
2018-02-20 08:12:24 -0600

    plot scripts modified

--- plots/createPlot.sh
+++ plots/createPlot.sh
@@ -7,14 +7,20 @@ fi
 
 cd $1/gnu
 cat Mpi.txt OpenMP.txt > ../results.txt
+cat Mpi_Pin.txt OpenMP_Pin.txt > ../results_Pin.txt
 
 cd ../intel
-cat Mpi.txt OpenMP.txt >> ../results.txt
+cat Mpi.txt OpenMP.txt > ../results.txt
+cat Mpi_Pin.txt OpenMP_Pin.txt >> ../results_Pin.txt
+
 cd ../pgi
-cat Mpi.txt OpenMP.txt >> ../results.txt
+cat Mpi.txt OpenMP.txt > ../results.txt
+cat Mpi_Pin.txt OpenMP_Pin.txt >> ../results_Pin.txt
 
 cd ../..
 
 
 gnuplot -c plot.gnp $1
+gnuplot -c plot_Pin.gnp $1
 
+rm `find . -name result*.txt`
