993c77f3f6b370e9649ceef9303ea87539e5658a
Edgar F. Black <efblack2@illinois.edu>
2018-02-22 10:40:07 -0600

    imptovments to plot scripts

--- plots/createPlot.sh
+++ plots/createPlot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]
+if [ "$#" -lt 1 ]
 then
-  echo "Usage: $0  computer"
+  echo "Usage: $0  computer [ylimit]"
   exit 1
 fi
 
@@ -20,10 +20,17 @@ cat Mpi_Pin.txt OpenMP_Pin.txt >> ../results_Pin.txt
 cd ../..
 
 
-gnuplot -c plot.gnp $1
-gnuplot -c plot_Pin.gnp $1
-gnuplot -c plotGnu.gnp $1
-gnuplot -c plotGnu_Pin.gnp $1
+gnuplot -c plot.gnp $1 $2
+gnuplot -c plot_Pin.gnp $1 $2
+mv $1.pdf temp.pdf
+pdfunite temp.pdf $1_pin.pdf $1.pdf
+rm temp.pdf $1_pin.pdf
+
+gnuplot -c plotGnu.gnp $1  $2
+gnuplot -c plotGnu_Pin.gnp $1  $2
+mv $1_Gnu.pdf temp.pdf
+pdfunite temp.pdf $1_GnuPin.pdf $1_Gnu.pdf
+rm  temp.pdf $1_GnuPin.pdf
 
 
 rm `find . -name result*.txt`
