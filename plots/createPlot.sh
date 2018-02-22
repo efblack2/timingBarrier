66fb7d2ddcee04deba3563716c21d86124032b6c
Edgar F. Black <efblack2@illinois.edu>
2018-02-22 16:39:22 -0600

    fixed script

--- plots/createPlot.sh
+++ plots/createPlot.sh
@@ -5,19 +5,6 @@ then
   exit 1
 fi
 
-cd $1/gnu
-cat Mpi.txt OpenMP.txt > ../results.txt
-cat Mpi_Pin.txt OpenMP_Pin.txt > ../results_Pin.txt
-
-cd ../intel
-cat Mpi.txt OpenMP.txt > ../results.txt
-cat Mpi_Pin.txt OpenMP_Pin.txt >> ../results_Pin.txt
-
-cd ../pgi
-cat Mpi.txt OpenMP.txt > ../results.txt
-cat Mpi_Pin.txt OpenMP_Pin.txt >> ../results_Pin.txt
-
-cd ../..
 
 
 gnuplot -c plot.gnp $1 $2
