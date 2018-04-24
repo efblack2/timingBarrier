602e961cabe3f45b0424e6871d5ed910380ce793
Edgar Black <efblack2@illinois.edu>
2018-04-24 08:19:03 -0500

    fixing loop size and some typos

--- MPI/src/main.c
+++ MPI/src/main.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>	
+#include <time.h>
 #include <unistd.h>
-#include <stdint.h>	
+#include <stdint.h>
 #include <mpi.h>
 
 #define BILLION 1000000000L
-//#define LOOP 10000000L
-#define LOOP 100000L
-
+#define LOOP 1000000L
 
 int main(int argc, char *argv[])
 {
 	struct timespec start, end;
-	uint64_t diff, barrirer_time=0, no_barrirer_time=0;
+	uint64_t diff, barrier_time=0, no_barrier_time=0;
     uint64_t max_iterations = LOOP;
 
     const int root=0;
@@ -35,14 +33,14 @@ int main(int argc, char *argv[])
 
 
     for (uint64_t n=0 ; n<max_iterations; ++n) {
-    
+
         MPI_Barrier(MPI_COMM_WORLD);
         clock_gettime(CLOCK_MONOTONIC, &start);
         MPI_Barrier(MPI_COMM_WORLD);
         clock_gettime(CLOCK_MONOTONIC, &end);
         diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
         MPI_Allreduce( MPI_IN_PLACE, &diff, 1, MPI_UNSIGNED_LONG, MPI_MIN,MPI_COMM_WORLD );
-        barrirer_time+=diff;
+        barrier_time+=diff;
 
 
         MPI_Barrier(MPI_COMM_WORLD);
@@ -51,16 +49,16 @@ int main(int argc, char *argv[])
         clock_gettime(CLOCK_MONOTONIC, &end);
         diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
         MPI_Allreduce( MPI_IN_PLACE, &diff, 1, MPI_UNSIGNED_LONG, MPI_MIN,MPI_COMM_WORLD );
-        no_barrirer_time+=diff;
-        
+        no_barrier_time+=diff;
+
     }	// end for //
-    
-    
+
+
     if (rank == root) {
         printf ("%lu is the Barrier estimate; %lu is total time with barrier; and %lu is the total time with no_barrier [ nano-seconds]",
-                  (barrirer_time-no_barrirer_time)/(max_iterations),
-                  barrirer_time/(max_iterations),
-                  no_barrirer_time/(max_iterations)
+                  (barrier_time-no_barrier_time)/(max_iterations),
+                  barrier_time/(max_iterations),
+                  no_barrier_time/(max_iterations)
         );
         printf(", number of processes: %d\n", size);
     } // end if
--- OpenMP/src/main.c
+++ OpenMP/src/main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>	
+#include <time.h>
 #include <unistd.h>
-#include <stdint.h>	
+#include <stdint.h>
 #include <omp.h>
 
 #define BILLION 1000000000L
-//#define LOOP 10000000L
-#define LOOP 100000L
+#define LOOP 1000000L
 
 int main(int argc, char *argv[])
 {
 	struct timespec start, end;
-	uint64_t barrirer_time=0, no_barrirer_time=0;
+	uint64_t barrier_time=0, no_barrier_time=0;
     uint64_t max_iterations = LOOP;
     uint64_t minimum;
     int size;
@@ -25,24 +24,24 @@ int main(int argc, char *argv[])
 
 
     #pragma omp parallel
-    {   
+    {
         #pragma omp master
         size=omp_get_num_threads();
     } // end of parallel region //
 
     uint64_t *diff = (uint64_t *) malloc(size * sizeof (uint64_t));
 
-    #pragma omp parallel  private(start, end ) //reduction(+:barrirer_time) reduction(+:no_barrirer_time)
-    {   
+    #pragma omp parallel  private(start, end ) //reduction(+:barrier_time) reduction(+:no_barrier_time)
+    {
         int rank = omp_get_thread_num();
-        
+
         for (uint64_t n=0 ; n<max_iterations; ++n) {
-        
+
             #pragma omp barrier
             clock_gettime(CLOCK_MONOTONIC, &start);
             #pragma omp barrier
             clock_gettime(CLOCK_MONOTONIC, &end);
-            
+
             diff[rank] = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
 
             #pragma omp barrier
@@ -54,16 +53,16 @@ int main(int argc, char *argv[])
                        minimum = diff[c];
                     } // end if //
                 }  // end for //
-                barrirer_time+=minimum;
+                barrier_time+=minimum;
             } // end of master region//
-            
+
             ////////////////////////////////
-        
+
             #pragma omp barrier
             clock_gettime(CLOCK_MONOTONIC, &start);
             // no barrirer here
             clock_gettime(CLOCK_MONOTONIC, &end);
-            
+
             diff[rank] = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
 
             #pragma omp barrier
@@ -75,16 +74,16 @@ int main(int argc, char *argv[])
                        minimum = diff[c];
                     } // end if //
                 }  // end for //
-                no_barrirer_time+=minimum;
+                no_barrier_time+=minimum;
             } // end of master region//
-            
+
         }	// end for//
     } // end of parallel region //
-    
-    printf ("%lu is the Barrirer estimate; %lu is total time with barrirer; and %lu is the total time with no_barrirer [ nano-seconds]",
-              (barrirer_time-no_barrirer_time)/(max_iterations),
-              barrirer_time/(max_iterations),
-              no_barrirer_time/(max_iterations)
+
+    printf ("%lu is the Barrier estimate; %lu is total time with barrier; and %lu is the total time with no_barrier [ nano-seconds]",
+              (barrier_time-no_barrier_time)/(max_iterations),
+              barrier_time/(max_iterations),
+              no_barrier_time/(max_iterations)
     );
     printf(", number of threads: %d\n", size);
 
